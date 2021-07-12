872c3014e3d2118202920e72dfdec27cb766c686
kdd9512 <hyunwoo8805@gmail.com>
2021-07-12 22:16:44 +0900

    commit 02 20210712
    cloning twitter with react, firebase

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [pw, setPw] = useState("");
+
+    const onChange = (e) => {
+        const {target: {name,value}} = e;
+        console.log(value)
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "pw") {
+            setPw(value);
+        }
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div>
+            <span>Please Log In</span>
+            <form onSubmit={onSubmit}>
+                <input
+                    name="email"
+                    type="text"
+                    placeholder="Your Email"
+                    required
+                    value={email}
+                    onChange={onChange}
+                />
+                <input
+                    name="pw"
+                    type="password"
+                    placeholder="Password"
+                    required
+                    value={pw}
+                    onChange={onChange}
+                />
+                <input type="submit" value="Log In"/>
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    )
+};
 export default Auth;
