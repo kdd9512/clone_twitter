bd25b74ec9230546ba435708378822652a8f151f
kdd9512 <hyunwoo8805@gmail.com>
2021-07-13 22:50:04 +0900

    commit 02 20210713
    cloning twitter with react, firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from "components/Router";
 import { authService } from "../fbase";
 
 function App() {
-    const [isLogin, setIsLogin] = useState(authService.currentUser);
+    const [init, setInit] = useState(false);
+    const [isLogin, setIsLogin] = useState(false);
+    useEffect(() => {
+        authService.onAuthStateChanged(
+            (user)=> {
+                if (user){
+                    setIsLogin(true);
+                } else {
+                    setIsLogin(false);
+                }
+                setInit(true);
+            });
+    },[])
   return (
     <>
-      <AppRouter isLogin={isLogin}/>
+        {init? <AppRouter isLogin={isLogin}/> : "Initializing"}
         <footer>&copy; {new Date().getFullYear()} clone_twitter </footer>
     </>
   );
