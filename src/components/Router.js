3603872b7b9a71157390d45508627816cc3d6f41
kdd9512 <hyunwoo8805@gmail.com>
2021-07-14 18:56:02 +0900

    commit 03 20210714
    cloning twitter with react, firebase

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,15 +2,21 @@ import React, {useState} from "react";
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navi from "./Navi";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLogin}) => {
     return (
         <Router>
+            {isLogin && <Navi/>}
             <Switch>
                 {isLogin ? (
                     <>
                         <Route exact path="/">
                             <Home/>
+                        </Route>
+                        <Route exact path="/profile">
+                            <Profile/>
                         </Route>
                     </>
                 ) : (
