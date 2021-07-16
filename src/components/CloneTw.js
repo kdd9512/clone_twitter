23fdb10f6d9e21a8841f57b81979c4632d1c5d85
kdd9512 <hyunwoo8805@gmail.com>
2021-07-16 21:43:29 +0900

    commit 04 20210716
    cloning twitter with react, firebase

--- src/components/CloneTw.js
+++ src/components/CloneTw.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import {dbService} from "../fbase";
 
 const CloneTw = ({cloneTwObj, isOwner}) => {
+    const [edit, setEdit] = useState(false); // 수정모드인가 아닌가를 따지기 위함.
+    const [newEdit, setNewEdit] = useState(cloneTwObj.text); // DB상의 값 cloneTwObj.text를 변경하기 위함.
+
     const onDeleteClick = async () => {
         const conf = window.confirm("Are you sure you want to delete this message?");
         if (conf) {
             await dbService.doc(`cloneTw/${cloneTwObj.id}`).delete();
         }
     };
+    const toggleEditing = () => setEdit((prev) => !prev);
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.doc(`cloneTw/${cloneTwObj.id}`).update({
+            text:newEdit,
+        });
+        setEdit(false);
+    };
+
+    const onChange = (e) => {
+        const {
+            target:{value},
+        } = e;
+        setNewEdit(value);
+    };
 
     return (
-        <div key={cloneTwObj.id}>
-            <h4>{cloneTwObj.text}</h4>
-            {isOwner && (
+        <div>
+            {edit ? (
                 <>
-                    <button onClick={onDeleteClick}>DELETE</button>
-                    <button>EDIT</button>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            onChange={onChange}
+                            type="text"
+                            placeholder="Edit this Message"
+                            value={newEdit}
+                            required/>
+                        <input type="submit" value="Update cloneTw" />
+                    </form>
+                    <button onClick={toggleEditing}>CANCEL</button>
+                </>
+            ) : (
+                <>
+                    <h4>{cloneTwObj.text}</h4>
+                    {isOwner && (
+                        <>
+                            <button onClick={onDeleteClick}>DELETE</button>
+                            <button onClick={toggleEditing}>EDIT</button>
+                        </>
+                    )}
                 </>
             )}
         </div>
