936f0114d0989e8f9eb495eb6b54c2fedd4fab5d
kdd9512 <hyunwoo8805@gmail.com>
2021-07-16 18:24:25 +0900

    commit 04 20210716
    cloning twitter with react, firebase

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
+import {dbService} from "../fbase";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [cloneTw, setCloneTw] = useState("");
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await dbService.collection("cloneTw").add({
+            cloneTw: cloneTw,
+            createdAt: Date.now(),
+        });
+        setCloneTw("");
+    };
+    const onChange = (e) => {
+        const {
+            target:{value},
+        } = e;
+        setCloneTw(value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={cloneTw}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What on your mind?"
+                    maxLength={120}
+                />
+                <input
+                    type="submit"
+                    value="cloneTw"
+                />
+            </form>
+        </div>
+    );
+};
+
 export default Home;
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,15 +2,19 @@ import React from "react";
 import {authService} from "../fbase";
 import {useHistory} from "react-router-dom";
 
-export default  () => {
+export default () => {
+
     const history = useHistory();
-const onLogOutClick = () => {
-    authService.signOut()
-    history.push("/");
-};
+    const onLogOutClick = () => {
+        authService.signOut()
+        history.push("/");
+    };
+
     return (
         <>
+            <span>My Profile</span>
+            <br/>
             <button onClick={onLogOutClick}>Sign Out</button>
         </>
     )
-}
+};
